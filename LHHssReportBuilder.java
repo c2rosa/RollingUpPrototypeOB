ae59a048df0d0e2fb1f97425a9db1ceb9e829210
rosa.charles <rosa.charles@con-way.com>
2018-06-18 12:26:09 -0400

    Modified to run the tariff leaf random forest model results.

--- LHHssReportBuilder.java
+++ LHHssReportBuilder.java
@@ -718,10 +718,10 @@ public class LHHssReportBuilder {
             rb.consolidateMachineLearningResults(myInputSubdirectory, myX_fileName, myY_fileName, myLeaf_fileName,delimiterForOutput);
         } else if(subroutineToRun.equals("rollUpMachineLearningLeafModelResultsResults")) {
 
-            String modelType = "AMC";
-            String myInputSubdirectory = "C:\\Old Drive\\rosa.charles\\workspace_git\\RevMan_python_dataAnalysis\\data\\data_AMC_Tariff_20180514_outputAndGeneratedInput\\"+modelType+"\\updatedData_optExistingModel_20180515\\ExpAmntOfSegSpecData_pGenLeaf\\";
+            String modelType = "Tariff";
+            String myInputSubdirectory = "C:\\Old Drive\\rosa.charles\\workspace_git\\RevMan_python_dataAnalysis\\data\\data_AMC_Tariff_20180514_outputAndGeneratedInput\\Tariff\\updatedData_optExistingModel_20180515\\ExpAmntOfSegSpecData_mods\\";
             String myFilePrefixToDetermineSegments = "leafOutputFile_fieldsConsidered_";
-            String myLeafModel_Results_output = "LeafModel_Results_AMC.csv";
+            String myLeafModel_Results_output = "LeafModel_Results_Tariff.csv";
 
             String delimiterForOutput = "\t";
             rb.rollUpMachineLearningLeafModelResultsResults(modelType, myInputSubdirectory, myFilePrefixToDetermineSegments, myLeafModel_Results_output);
